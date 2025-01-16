3d26860417e77bdbe5eb4e9c9e6099ee3c1c44ad
serg10arg <cabrerasergio094@gmail.com>
2025-01-15 23:18:49 -0300

    implementacion de relaciones entre beans

---
+++ Projects/4-Implementing-relationships-among-beans-defined-in-the-configuration-file/src/main/java/org/example/Parrot.java
@@ -0,0 +1,26 @@
+package org.example;
+
+public class Parrot {
+    private String name;
+
+    public Parrot() {
+
+    }
+
+    public Parrot(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Parrot: " + name;
+    }
+}
---
+++ Projects/4-Implementing-relationships-among-beans-defined-in-the-configuration-file/src/main/java/org/example/ProjectConfig.java
@@ -0,0 +1,21 @@
+package org.example;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ProjectConfig {
+    @Bean
+    public Parrot parrot() {
+        Parrot parrot = new Parrot();
+        parrot.setName("Koko");
+        return  parrot;
+    }
+
+    @Bean
+    public Person person() {
+        Person person = new Person();
+        person.setName("Ella");
+        return person;
+    }
+}
