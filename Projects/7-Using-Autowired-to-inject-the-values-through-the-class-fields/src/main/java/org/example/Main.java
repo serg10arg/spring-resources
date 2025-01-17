0d760458126500f05777f55f150f9b3bd4e10d65
serg10arg <cabrerasergio094@gmail.com>
2025-01-17 18:41:54 -0300

    usando anotacion Autowired para inyectar beans

---
+++ Projects/7-Using-Autowired-to-inject-the-values-through-the-class-fields/src/main/java/org/example/Main.java
@@ -0,0 +1,20 @@
+package org.example;
+
+import beans.Person;
+import beans.ProjectConfig;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class Main {
+    public static void main(String[] args) {
+        // Crea el contexto de Spring usando la configuración especificada en ProjectConfig
+        var context = new AnnotationConfigApplicationContext(ProjectConfig.class);
+
+        // Obtener el bean de un tipo Person
+        Person person = context.getBean(Person.class);
+
+        // Imprimir información de la persona y su loro
+        System.out.println("Person's name: " + person.getName());
+        System.out.println("Person's parrot: " + person.getParrot());
+
+    }
+}
