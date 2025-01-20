a1f039fcb01193537a94054707766bdc853ad085
serg10arg <cabrerasergio094@gmail.com>
2025-01-20 20:58:48 -0300

    utilizando @ Qualifier para elegir entre multiples @ beans

---
+++ Projects/03-The_Spring_context_wiring_beans/08-sq-ch3-ex8/src/main/java/config/ProjectConfig.java
@@ -0,0 +1,36 @@
+package config;
+
+import beans.Parrot;
+import beans.Person;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ProjectConfig {
+
+    @Bean
+    public Parrot parrot1() {
+        Parrot p = new Parrot();
+        p.setName("Koko");
+        return p;
+    }
+
+    @Bean
+    public Parrot parrot2() {
+        Parrot p = new Parrot();
+        p.setName("Miki");
+        return p;
+    }
+
+    // @Qualifier permite especificar explícitamente el bean que deseas inyectar
+    // Esto elimina la dependencia del nombre del parámetro
+    @Bean
+    public Person person(@Qualifier("parrot2") Parrot parrot) {
+        Person p = new Person();
+        p.setName("Ella");
+        p.setParrot(parrot);
+        return p;
+
+    }
+}
