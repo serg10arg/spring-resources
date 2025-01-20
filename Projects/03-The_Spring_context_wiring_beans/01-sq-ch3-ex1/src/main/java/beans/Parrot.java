7c3d9725f058c5855935bd354d2ba7dd242d8d19
serg10arg <cabrerasergio094@gmail.com>
2025-01-20 17:22:52 -0300

    agregar beans al contexto de spring

---
+++ Projects/03-The_Spring_context_wiring_beans/01-sq-ch3-ex1/src/main/java/beans/Parrot.java
@@ -0,0 +1,26 @@
+package beans;
+
+public class Parrot {
+    private String name;
+
+    public Parrot() {
+
+    }
+
+    public Parrot(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Parrot: " + name;
+    }
+}
