6d9b3c735ea2c03c6626e25639d49854ac7f2ae4
serg10arg <cabrerasergio094@gmail.com>
2025-02-02 19:49:44 -0300

    delete ex2

--- Projects/12-Using-data-sources-in-Spring-apps/02-sq-ch12-ex2/src/main/resources/schema.sql
+++
@@ -1,7 +0,0 @@
-  DROP TABLE IF EXISTS purchase;
-
-  CREATE TABLE IF NOT EXISTS purchase (
-      id INT AUTO_INCREMENT PRIMARY KEY,
-      product varchar(50) NOT NULL,
-      price double NOT NULL
-  );
